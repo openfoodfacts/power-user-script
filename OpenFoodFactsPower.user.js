c7da8e3f96d76174f19a07d186274fc4f696172a
Charles Nepote <charles@nepote.org>
2019-10-23 14:50:44 +0200

    number of products easier to read

--- OpenFoodFactsPower.user.js
+++ OpenFoodFactsPower.user.js
@@ -2,7 +2,7 @@
 // @name        Open Food Facts power user script
 // @description Helps power users in their day to day work. Key "?" shows help. This extension is a kind of sandbox to experiment features that could be added to Open Food Facts website.
 // @namespace   openfoodfacts.org
-// @version     2019-09-12T16:45
+// @version     2019-10-23T13:42
 // @include     https://*.openfoodfacts.org/*
 // @include     https://*.openproductsfacts.org/*
 // @include     https://*.openbeautyfacts.org/*
@@ -23,7 +23,7 @@
 (function() {
     'use strict';
     const pageType = isPageType(); // test page type
-    console.log("2019-09-12T16:45 - mode: " + pageType);
+    console.log("2019-10-23T13:42 - mode: " + pageType);
 
     // Disable extension if the page is an API result; https://world.openfoodfacts.org/api/v0/product/3222471092705.json
     if (pageType === "api") {
@@ -57,6 +57,7 @@
     //   * better distinguished sections
     //   * fields highlighted, current field highlighted
     //   * less margins for some elements
+    //   * number of products easier to read (with separators depending on your locale); see: https://github.com/openfoodfacts/openfoodfacts-server/issues/2474
     // * UI
     //   * help screen called with button [?] or keyboard shortcut (?) or (h)
     //   * zoom every images with mouse wheel; see http://www.jacklmoore.com/zoom/
@@ -507,6 +508,12 @@ margin: 0 0 0.2rem 0 !important;
 #timed_alert { position:fixed; top:0; right:0; font-size: 8rem }
 
 `;
+        var xxxProducts = $(".button-group li div").text(); console.log(xxxProducts); // 1009326 products
+        var nbOfProducts = parseInt(xxxProducts.match(/(\d+)/g)[0]); //console.log(nbOfProducts); // 1009326
+        nbOfProducts = nbOfProducts.toLocaleString(); //console.log(nbOfProducts); // 1 009 326
+        $(".button-group li div").text(xxxProducts.replace(/(\d+)(.*)/, nbOfProducts+"$2")); // 1 009 326 products
+
+
         var listByRowsMode = false; // We are not yet in "list by rows" mode
         // Keyboard actions
         if (listByRowsOption === true) { listByRows(); }
