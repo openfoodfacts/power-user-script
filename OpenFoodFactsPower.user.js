35b6e976442eb71f7d0745234c60800c32853b13
Charles Nepote <charles@nepote.org>
2019-11-18 18:05:56 +0100

    Fix crowdin link with page language

--- OpenFoodFactsPower.user.js
+++ OpenFoodFactsPower.user.js
@@ -2,7 +2,7 @@
 // @name        Open Food Facts power user script
 // @description Helps power users in their day to day work. Key "?" shows help. This extension is a kind of sandbox to experiment features that could be added to Open Food Facts website.
 // @namespace   openfoodfacts.org
-// @version     2019-11-18T16:53
+// @version     2019-11-18T16:54
 // @include     https://*.openfoodfacts.org/*
 // @include     https://*.openproductsfacts.org/*
 // @include     https://*.openbeautyfacts.org/*
@@ -23,7 +23,7 @@
 (function() {
     'use strict';
     const pageType = isPageType(); // test page type
-    console.log("2019-11-18T16:53 - mode: " + pageType);
+    console.log("2019-11-18T16:54 - mode: " + pageType);
 
     // Disable extension if the page is an API result; https://world.openfoodfacts.org/api/v0/product/3222471092705.json
     if (pageType === "api") {
@@ -207,9 +207,14 @@ color: #00f;
     // *
     // Add quick links in the sidebar: page translation, category translation, Recent Changes...
     if (pageType !== "api") {
+        var pageLanguage = $("html").attr('lang');      // Get page language
+        console.log("Page language: " + pageLanguage);
+        if(pageLanguage === "en") {                     // Delete page language if "en" because we can't make the difference bewteen "en-GB" and "en-US"
+            pageLanguage = "";
+        }
         $(".sidebar p:first").after(
             '<p>'+
-            '> <a href="https://crowdin.com/project/openfoodfacts">' +
+            '> <a href="https://crowdin.com/project/openfoodfacts/'+pageLanguage+'">' +
             'Help page translation</a></p>'+
             '<p>'+
             '> <a href="/categories?translate=1">' +
