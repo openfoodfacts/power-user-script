f045d4ccf7a9753f99512d792e9e3b8ffb131996
Charles Nepote <charles@openfoodfacts.org>
2022-11-08 11:24:44 +0100

    Add openfoodfacts.net

--- OpenFoodFactsPower.user.js
+++ OpenFoodFactsPower.user.js
@@ -2,12 +2,13 @@
 // @name        Open Food Facts power user script
 // @description Helps power users in their day to day work. Key "?" shows help. This extension is a kind of sandbox to experiment features that could be added to Open Food Facts website.
 // @namespace   openfoodfacts.org
-// @version     2022-10-19T15:11
+// @version     2022-11-08T11:20
 // @include     https://*.openfoodfacts.org/*
 // @include     https://*.openproductsfacts.org/*
 // @include     https://*.openbeautyfacts.org/*
 // @include     https://*.openpetfoodfacts.org/*
 // @include     https://*.pro.openfoodfacts.org/*
+// @include     https://*.openfoodfacts.net/*
 // @include     https://*.openfoodfacts.dev/*
 // @include     http://*.productopener.localhost/*
 // @exclude     https://analytics.openfoodfacts.org/*
@@ -54,7 +55,7 @@
     var proPlatform = false;     // TODO: to be included in isPageType()
     const pageType = isPageType(); // test page type
     const corsProxyURL = "";
-    log("2022-10-19T15:11 - mode: " + pageType);
+    log("2022-11-08T11:20 - mode: " + pageType);
 
     // Disable extension if the page is an API result; https://world.openfoodfacts.org/api/v0/product/3222471092705.json
     if (pageType === "api") {
