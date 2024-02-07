f230088e8eba9594563619ce09faf8d94de7cd39
Danielius Ašmontas <dan.asmontas@gmail.com>
2024-02-07 09:01:18 +0200

    Update OpenFoodFactsPower.user.js
    Co-authored-by: Charles Nepote <charles@openfoodfacts.org>

--- OpenFoodFactsPower.user.js
+++ OpenFoodFactsPower.user.js
@@ -2014,7 +2014,7 @@ ul#products_match_all > li > a > span { display: table-cell; width:   70%;  vert
             let barcode = $(this).attr('data-code');
             $(this).append('<a class="list_hunger_games_logo_search" alt="Hunger games logo search" title="Hunger games logo search" href="https://hunger.openfoodfacts.org/logos/search?barcode='+barcode+'"><span class="material-icons">image_search</span></a>');
             $(this).append('<a class="list_rotate_image_270" alt="Rotate at -90°" title="Rotate at -90°"><span class="material-icons"  style="transform: scaleX(-1);">redo</span></a>');
-            $(this).append('<a class="list_rotate_image_180" alt="Rotate at 180°" title="Rotate at 180°"><span class="material-icons">rotate_left</span></a>');
+            $(this).append('<a class="list_rotate_image_180" alt="Rotate at 180°" title="Rotate at 180°"><span class="material-icons">rotate_right</span></a>');
             $(this).append('<a class="list_rotate_image_90" alt="Rotate at 90°" title="Rotate at 90°"><span class="material-icons" style="transform: rotate(90deg);">rotate_right</span></a>'); 
 
             var image_reference = $(".list_product_img", $(this)); 
